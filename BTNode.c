7457e3f015b3bb0336e89968e97d63603b796d77
Wuchong123456 <68960535+Wuchong123456@users.noreply.github.com>
2021-01-06 22:47:59 +0800

    Update BTNode.c

--- BTNode.c
+++ BTNode.c
@@ -56,9 +56,88 @@ int BinaryTreeSize(BTNode* root)// 二叉树叶子节点个数
 	{
 		return 1;
 	}
-	else
+	
+	return BinaryTreeSize(root->_left) + BinaryTreeSize(root->_right);
+}
+int BinaryTreeLevelKSize(BTNode* root, int k)// 二叉树第k层节点个数
+{
+	if (root == NULL)
 	{
 		return 0;
 	}
-	return BinaryTreeSize(root->_left) + BinaryTreeSize(root->_right);
+	if (k == 1)
+	{
+		return 1;
+	}
+	return BinaryTreeLevelKSize(root->_left, k - 1) + BinaryTreeLevelKSize(root->_right, k - 1);
+}
+
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x)// 二叉树查找值为x的节点
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	if (root->_data == x)
+	{
+		return root;
+	}
+	BTNode* Left = BinaryTreeFind(root->_left, x);
+	if (Left == NULL)
+	{
+		BTNode* Right = BinaryTreeFind(root->_right, x);
+		return Right;
+
+	}
+	return Left;
+}
+void _BinaryTreePrevOrder(BTNode* root,int* arr,int* i)//前序遍历
+{
+	
+	if (root == NULL)
+	{
+		return;
+	}
+	else
+	{
+		arr[*i] = root->_data;
+		(*i)++;
+		_BinaryTreePrevOrder(root->_left, arr,i);
+		_BinaryTreePrevOrder(root->_right, arr, i);
+	}
+	
+}
+
+void _BinaryTreeInOrder(BTNode* root, int* arr, int* i)// 二叉树中序遍历
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	else
+	{
+		_BinaryTreeInOrder(root->_left, arr, i);
+		arr[*i] = root->_data;
+		(*i)++;
+		_BinaryTreeInOrder(root->_right, arr, i);	
+	}
+
+}
+
+void BinaryTreePrevOrder(BTNode* root)// 二叉树前序遍历
+{
+	int Size = BinaryTreeSize(root);
+	int* arr = malloc(sizeof(int)*Size);
+	int i = 0;
+	_BinaryTreePrevOrder(root, arr, &i);
+	for (i=0; i < Size; i++)
+	{
+		printf("%c ", arr[i]);
+	}
+	printf("\n");
+	_BinaryTreeInOrder(root, arr, &i);
+	for (i = 0; i < Size; i++)
+	{
+		printf("%c ", arr[i]);
+	}
 }
